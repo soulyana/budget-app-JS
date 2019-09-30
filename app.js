519007899ff24a54c402f5649a99201fe5301bdf
Soulyana <soulyana.lakew@accenturefederal.com>
2019-09-30 15:15:15 -0400

    Project planning and arcitechture

---
+++ app.js
@@ -0,0 +1,34 @@
+/**
+ * Modules
+ * Important aspect of any robust application's architecutre
+ * Keep the units of code for a project both cleanly separated and organized
+ * Encapsulate some data into privacy and expose other data publicly
+ */ 
+
+ /**
+  * To-Do List
+  * Add event handler
+  * Get input values
+  * add the new item to our data structure
+  * Add the new item to the UI
+  * Calculate the budget
+  * Update the UI
+  */
+
+  /**
+   * UI Module
+   * Get input values
+   * Add the new item to the UI
+   * Update the UI
+   */
+
+   /***
+    * Data Module
+    * Add the new item to our data structure
+    * Calculate the budget
+    */
+
+    /**
+     * Controller Module
+     * Add event handler
+     */
