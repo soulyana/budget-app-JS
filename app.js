c26bc2bc2a76ef185fe8c91bf1eb389e74b3cf58
Soulyana <soulyana.lakew@accenturefederal.com>
2019-10-01 13:04:19 -0400

    Setting up the First Event Listeners

--- app.js
+++ app.js
@@ -39,38 +39,50 @@
      * More about private and public data, encapsulation, and separation of concerns
      */
 
+     // BUDGET CONTROLLER
      var budgetController = (function() {
 
-        var x = 23;
+        // some code
 
-        var add = function(a) {
-            return x + a;
-        }
-
-        return {
-            publicTest: function(b) {
-                return add(b);
-            }
-        }
      })();
 
-     console.log(budgetController.publicTest(5));
-
+     // UI CONTROLLER
      var UIController = (function() {
 
         /// Some code
+
      })();
 
+      /**
+      * Setting up the First Event Listeners
+      * how to set up event listeners for keypress events
+      * how to use event object
+      */
+
+     // GLOBAL APP CONTROLLER
      var controller = (function(budgetCtrl, UICtrl) {
 
-        var z = budgetCtrl.publicTest(5);
+        var ctrlAddItem = function() {
+            // 1. Get the field input data
 
-        return {
-            anotherPublic: function() {
-                console.log(z);
-            }
+            // 2. Add the item to the budget controller
+
+            // 3. Add the new item to the user interface
+
+            // 4. Calculate the budget
+
+            // 5. Display the budget on the UI
+            console.log('it works');
         }
+        document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function(event) {
+
+            if (event.keyCode === 13 || event.which === 13) { //event.which for older browsers
+                ctrlAddItem();
+            }
+        });
 
      })(budgetController, UIController);
 
-     
+    
