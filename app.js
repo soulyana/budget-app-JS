bbaca8f6c0b7b25e9c95cdb964681cb0e935d332
Soulyana <soulyana.lakew@accenturefederal.com>
2019-10-02 11:24:56 -0400

    Updating the Budget: Budget Controller

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /**
- *  Updating the Budget: Controller
- *  How to convert field inputs to numbers
- *  How to prevent false inputs
+ *  Updating the Budget: Budget Controller
+ *  How and why to create simple, reusable functions with only one purpose
+ *  How to sum all elements of an array using the forEach method
  */
 
 // BUDGET CONTROLLER
@@ -19,6 +19,14 @@ var budgetController = (function () {
         this.value = value;
     };
 
+    var calculateTotal = function(type) {
+        var sum = 0;
+        data.allItems[type].forEach(function(cur) {
+            sum += cur.value;
+        });
+        data.totals[type] = sum;
+    };
+
     var data = {
         allItems: {
             exp: [],
@@ -27,7 +35,9 @@ var budgetController = (function () {
         totals: {
             exp: 0,
             inc: 0
-        }
+        },
+        budget: 0,
+        percentage: -1
     };
 
     return {
@@ -54,6 +64,31 @@ var budgetController = (function () {
             // Return the new element
             return newItem;
 
+        },
+
+        calculateBudget: function() {
+            // Calculate total income and expenses
+            calculateTotal('exp');
+            calculateTotal('inc');
+
+            // Calculate the budget: income - expense
+            data.budget = data.totals.inc - data.totals.exp;
+
+            // calculate the percentage of income that we spent
+            if (data.totals.inc > 0) {
+                data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+            } else {
+                data.percentage = -1;
+            }
+        },
+
+        getBudget: function() {
+            return {
+                budget: data.budget,
+                totalInc: data.totals.inc,
+                totalExp: data.totals.exp,
+                percentage: data.percentage
+            };
         },
 
         testing: function () {
@@ -145,11 +180,14 @@ var controller = (function (budgetCtrl, UICtrl) {
     var updateBudget = function () {
 
         // 1. Calculate the budget
+        budgetCtrl.calculateBudget();
 
         // 2. Return the budget
+        var budget = budgetCtrl.getBudget();
 
         // 3. Display the budget on the UI
-    }
+        console.log(budget);
+    };
 
     var ctrlAddItem = function () {
         var input, newItem;
