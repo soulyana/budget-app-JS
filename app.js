7156c666d40e25ad9e306571bbc088101c295a1e
Soulyana <soulyana.lakew@accenturefederal.com>
2019-10-03 14:26:51 -0400

    Updating the Budget: UI Controller

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 /**
- *  Updating the Budget: Budget Controller
- *  How and why to create simple, reusable functions with only one purpose
- *  How to sum all elements of an array using the forEach method
+ *  Updating the Budget: UI Controller 
+ *  Practice DOM manipulation by updating the budget and total values
  */
 
 // BUDGET CONTROLLER
@@ -106,7 +105,11 @@ var UIController = (function () {
         inputValue: '.add__value',
         inputButton: '.add__btn',
         incomeContainer: '.income__list',
-        expenseContainer: '.expenses__list'
+        expenseContainer: '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expenseLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage'
     };
 
     return {
@@ -152,6 +155,19 @@ var UIController = (function () {
             fieldsArr[0].focus();
         },
 
+        displayBudget: function(obj) {
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.expenseLabel).textContent = obj.totalExp;
+          
+            if (obj.percentage > 0) {
+                document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+            } else {
+                document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+
+            }
+        },
+
         getDOMstrings: function () {
             return DOMstrings;
         }
@@ -186,7 +202,7 @@ var controller = (function (budgetCtrl, UICtrl) {
         var budget = budgetCtrl.getBudget();
 
         // 3. Display the budget on the UI
-        console.log(budget);
+        UICtrl.displayBudget(budget);
     };
 
     var ctrlAddItem = function () {
@@ -216,6 +232,12 @@ var controller = (function (budgetCtrl, UICtrl) {
     return {
         init: function () {
             console.log('Application has started');
+            UICtrl.displayBudget({
+                budget: 0,
+                totalInc: 0,
+                totalExp: 0,
+                percentage: -1
+            });
             setupEventListeners();
         }
     };
