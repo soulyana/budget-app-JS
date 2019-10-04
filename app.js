f36b94b96b16462c8771dcb85c9aa0f28bf02914
Soulyana <soulyana.lakew@accenturefederal.com>
2019-10-03 20:57:53 -0400

    Finishing Touches: Improving the UX

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /**
- *  Displaying the current month and year
- *  How to get the current date by using the Date object constructor
+ *  Finishing Touches: Improving the UX
+ *  How and when to use 'change' events
  */
 
 // BUDGET CONTROLLER
@@ -189,6 +189,12 @@ var UIController = (function () {
              return (type === 'exp' ? '-' : '+') + ' ' + int + '.' + dec; 
         };
 
+        var  nodeListForEach = function(list, callback) {
+                for (var i = 0; i < list.length; i++) {
+                    callback(list[i], i);
+                }
+            };
+
     return {
         getInput: function () {
             return {
@@ -258,12 +264,6 @@ var UIController = (function () {
 
             var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
 
-            var  nodeListForEach = function(list, callback) {
-                for (var i = 0; i < list.length; i++) {
-                    callback(list[i], i);
-                }
-            };
-
             nodeListForEach(fields, function(current, index) {
                 
                 if (percentages[index] > 0) {
@@ -285,6 +285,19 @@ var UIController = (function () {
             document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
 
 
+        },
+
+        changedType: function() {
+            var fields = document.querySelectorAll(
+                DOMstrings.inputType + ',' +
+                DOMstrings.inputDescription + ',' +
+                DOMstrings.inputValue);
+                
+            nodeListForEach(fields, function(cur) {
+                cur.classList.toggle('red-focus');
+            });
+
+            document.querySelector(DOMstrings.inputButton).classList.toggle('red');
         },
 
         getDOMstrings: function () {
@@ -311,6 +324,8 @@ var controller = (function (budgetCtrl, UICtrl) {
         });
 
         document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+
+        document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
     };
 
     var updateBudget = function () {
